78cc163dc491bfa12b8a27b664597b0b1bf4f89e
Callum Orkney <callum.orkney@gmail.com>
2020-12-13 17:44:12 +0000

    Added density plot and boxplots for lines by character

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -31,16 +31,16 @@ lines_clean <- lines %>%
     )
 
 # Summarise number of lines by character, by episode (limit to six main characters)
-
 lines_by_character <- lines_clean %>%
   filter(speaker %in% names_six) %>%
-  group_by(ep_id, ep_no, speaker) %>% 
+  group_by(season, ep_id, ep_no, speaker) %>% 
   summarise(lines = n()) %>%
   ungroup() %>% 
   group_by(speaker) %>%
   mutate(movav = roll_meanr(lines, 30)) %>%
   ungroup()
 
+# Line plot showing moving average of lines per episode by character over time
 ggplot(lines_by_character, aes(x = ep_no, y = movav, colour = speaker)) +
   geom_line() +
   labs(
@@ -49,5 +49,31 @@ ggplot(lines_by_character, aes(x = ep_no, y = movav, colour = speaker)) +
     x = 'Episode number', y = 'Lines spoken per episode',
     colour = 'Character'
   )
+
+# Density plot comparing characters' numbers of lines, faceted by season
+ggplot(lines_by_character, aes(x = lines, colour = speaker)) +
+  geom_line(stat = "density") +
+  facet_wrap(~ season)
+
+# Same information shown as a boxplot
+ggplot(lines_by_character, aes(x = lines, y = speaker, colour = speaker)) +
+  geom_boxplot() +
+  facet_wrap(~ season) +
+  theme(
+    axis.title.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank()
+  )
+
+# Boxplot for all seasons combined
+ggplot(lines_by_character,
+       aes(x = lines, y = reorder(speaker, lines, median), colour = speaker)) +
+  geom_boxplot() +
+  theme(
+    legend.position = "none"
+  )
+
+
+
 
 
