e5cb063b60924235d2254d19fcc9f7ce73ca8142
Callum Orkney <callum.orkney@gmail.com>
2020-12-13 16:52:55 +0000

    Count lines by episode and character and make a moving-average line plot

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -3,6 +3,12 @@ library(dplyr)
 library(forcats)
 library(tidytext)
 library(stringr)
+library(ggplot2)
+library(RcppRoll)
+
+# Set constants
+names_six <- c('Monica Geller', 'Joey Tribbiani', 'Chandler Bing',
+               'Phoebe Buffay', 'Ross Geller', 'Rachel Green')
 
 # Get data
 
@@ -23,3 +29,25 @@ lines_clean <- lines %>%
     ep_no = cummax(as.numeric(ep_id)),
     line_id = paste(season, episode, scene, utterance, sep = '-')
     )
+
+# Summarise number of lines by character, by episode (limit to six main characters)
+
+lines_by_character <- lines_clean %>%
+  filter(speaker %in% names_six) %>%
+  group_by(ep_id, ep_no, speaker) %>% 
+  summarise(lines = n()) %>%
+  ungroup() %>% 
+  group_by(speaker) %>%
+  mutate(movav = roll_meanr(lines, 30)) %>%
+  ungroup()
+
+ggplot(lines_by_character, aes(x = ep_no, y = movav, colour = speaker)) +
+  geom_line() +
+  labs(
+    title = 'Number of lines per episode by main character',
+    subtitle = '30-episode moving averages',
+    x = 'Episode number', y = 'Lines spoken per episode',
+    colour = 'Character'
+  )
+
+
