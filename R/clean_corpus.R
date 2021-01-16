aefe2e0ae2a1f5548666be6e8c6d0f3b4f36e141
Callum Orkney <callum.orkney@gmail.com>
2021-01-16 11:06:54 +0000

    Added step to remove stopwords, and added stops as an argument to accomodate this

--- R/clean_corpus.R
+++ R/clean_corpus.R
@@ -7,9 +7,11 @@
 #' 
 
 
-clean_corpus <- function(corp) {
+clean_corpus <- function(corp, stops = tm::stopwords("en")) {
   corp %>%
     tm::tm_map(content_transformer(tolower)) %>%
+    tm::tm_map(tm::removeWords, stops) %>% 
     tm::tm_map(tm::removePunctuation) %>%
     tm::tm_map(tm::stripWhitespace)
+    
 }
