7790784b85b6c98486f5c30f6e5fbce79db17d91
Callum Orkney <callum.orkney@gmail.com>
2021-01-16 10:53:01 +0000

    Added custom stopwords

--- exploratory_analysis_corpus.R
+++ exploratory_analysis_corpus.R
@@ -2,10 +2,19 @@ library(tidytuesdayR)
 library(dplyr)
 library(tm)
 
+source("R/clean_corpus.R")
+
 # Set constants for analysis ----------------------------------------------
 
 names_six <- c('Monica Geller', 'Joey Tribbiani', 'Chandler Bing',
                'Phoebe Buffay', 'Ross Geller', 'Rachel Green')
+
+custom_stopwords <- c("yeah", "hey", "um", "uh", "ah", "umm",
+                      "y'know", "ya", "ohh", "gonna", "wanna",
+                      "ross", "rachel", "monica", "chandler", "joey",
+                      "phoebe", "huh", "ooh", "uhm", "wow", "woah", "whoa",
+                      "guys", "god", "fine", "gotta", "pheebs", "rach",
+                      stopwords("en"))
 
 
 # Get the data ------------------------------------------------------------
@@ -29,7 +38,6 @@ lines_clean <- lines %>%
   )
 
 
-
 # Create corpus where each document is a character ------------------------
 
 char_corpus <- lines_clean %>% 
@@ -39,3 +47,5 @@ char_corpus <- lines_clean %>%
   rename(doc_id = speaker) %>%
   DataframeSource() %>%
   VCorpus()
+
+corpus_clean <- clean_corpus(char_corpus)
